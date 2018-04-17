0979b57dbddd74cf538d477861013241b6df7803
kilimondjaro <kirill.babich@mail.ru>
2018-04-17 14:30:13 +0300

    Add neural network

---
+++ InFocusGame/Controllers/GameTypeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameTypeViewController.swift
+//  InFocusGame
+//
+//  Created by Kirill Babich on 17/04/2018.
+//  Copyright © 2018 Kirill Babich. All rights reserved.
+//
+
+import UIKit
+
+class GameTypeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
