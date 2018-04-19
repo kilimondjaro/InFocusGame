aef99fade6943b2d2849f7fd7862892c87f8a518
kilimondjaro <kirill.babich@mail.ru>
2018-04-19 09:25:34 +0300

    Replace TableViewController

---
+++ InFocusGame/Controllers/ObjectsListsViewController.swift
@@ -0,0 +1,116 @@
+//
+//  ObjectsListsViewController.swift
+//  InFocusGame
+//
+//  Created by Kirill Babich on 19/04/2018.
+//  Copyright © 2018 Kirill Babich. All rights reserved.
+//
+
+import UIKit
+
+class ObjectsListsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private var flatObjectsData: [String: Bool] = [:]
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let result = CoreDataManager.instance.fetch(entity: "FlatObjects")
+        
+        for data in result {
+            for name in Constants.flatObjects {
+                flatObjectsData[name] = data.value(forKey: name) as? Bool
+            }
+        }
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.reloadData()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return Constants.flatObjects.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "objectsListsCell", for: indexPath) as! ObjectsListsTableViewCell
+        // TODO - change for many sections
+        // TODO - add localization
+        
+        let obj = Constants.flatObjects[indexPath.row]
+        cell.objectSwitch.isOn = flatObjectsData[obj]!
+        cell.objectLabel.text =  obj
+        cell.objectName = obj
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        // TODO - for many sections
+        
+        return "Flat Objects"
+    }
+    
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+
+}
